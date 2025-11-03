89145ebb0f59a7480203954ccc3c3ea19eb94489
VietAnh <70483484+nguyenvietanh-abc@users.noreply.github.com>
2025-11-03 13:43:06 +0700

    Refactor ClientHandler to use shared_ptr for generator
    RAW, Thread_Safe

--- server/ClientHandler.h
+++ server/ClientHandler.h
@@ -1,15 +1,19 @@
+// server/ClientHandler.h
 #ifndef CLIENTHANDLER_H
 #define CLIENTHANDLER_H
 
 #include <QObject>
 #include <QTcpSocket>
+#include <memory>
 #include "SensorDataGenerator.h"
 
 class ClientHandler : public QObject
 {
     Q_OBJECT
 public:
-    explicit ClientHandler(QTcpSocket *socket, QObject *parent = nullptr);
+    explicit ClientHandler(std::shared_ptr<SensorDataGenerator> generator,
+                           QTcpSocket *socket,
+                           QObject *parent = nullptr);
     ~ClientHandler();
 
 private slots:
@@ -17,8 +21,10 @@ private slots:
     void onDisconnected();
 
 private:
-    QTcpSocket *socket;
-    SensorDataGenerator generator;
+    void sendSensorData();
+
+    QTcpSocket *const m_socket;
+    std::shared_ptr<SensorDataGenerator> m_generator;
 };
 
 #endif // CLIENTHANDLER_H
