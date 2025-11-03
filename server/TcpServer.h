b38840ba05925a3f1cadfc3b7a082b2e8a97699b
VietAnh <70483484+nguyenvietanh-abc@users.noreply.github.com>
2025-11-03 13:43:52 +0700

    Refactor TcpServer to include SensorDataGenerator

--- server/TcpServer.h
+++ server/TcpServer.h
@@ -1,18 +1,24 @@
+// server/TcpServer.h
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
 #include <QTcpServer>
-#include "ClientHandler.h"
+#include <memory>
+#include "SensorDataGenerator.h"
 
 class TcpServer : public QTcpServer
 {
     Q_OBJECT
 public:
     explicit TcpServer(QObject *parent = nullptr);
-    bool start(quint16 port);
+    ~TcpServer();
 
-protected:
+    bool startServer(quint16 port = 5000);
+
+private:
     void incomingConnection(qintptr socketDescriptor) override;
+
+    std::shared_ptr<SensorDataGenerator> m_generator;
 };
 
 #endif // TCPSERVER_H
